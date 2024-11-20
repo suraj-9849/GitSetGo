e99da5eeaba4d6cc9b5e188d9687e889601ecdf2
suraj <surajlohit42@gmail.com>
2024-11-20 10:58:16 +0530

    smalle changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-export default function App() {
+import React from 'react'
+import "./index.css"
+import Routing from './Routings/Routing'
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='overflow-hidden montserrat '>
+      <Routing/>
+    </div>
   )
 }
+
+export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+    I am Home Page  
+    </div>
+  )
+}
+
+export default Home
