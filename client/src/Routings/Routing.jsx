4809d68802fcdae9dff3c6c0ae8f9dd396591898
suraj <surajlohit42@gmail.com>
2024-11-20 17:41:59 +0530

    hello

--- client/src/Routings/Routing.jsx
+++ client/src/Routings/Routing.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../components/Home/Home'
+// import RoomPage from "./components/Room/Room";
 import Login from '../components/Authpages/Login'
 import Signup from '../components/Authpages/Signup'
-import VideoRoom from '../components/VideoRoom/VideoRoom'
 import ImaggaImageAnalysis from '../components/Ai/ImaggaImageAnalysis'
+import RoomPage from '../components/Room/RoomPage'
+import VideoCam from '../components/VideoCam/VideoCam'
 // import Profile from '../components/Profile/Profile'
 
 function Routing() {
   return (
     <div>
       <Routes>
-       <Route path="/" element={<Home />} />
-       {/* <Route path="/profile" element={<Profile />} /> */}
-       <Route path="/login" element={<Login />} />
-       <Route path="/sign-up" element={<Signup />} />
-        <Route path='/videoCam' element={<VideoRoom />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path='/videoCam' element={<VideoCam />} />
         <Route path='/image' element={<ImaggaImageAnalysis />} />
+        <Route path="/videocam/room/:roomId" element={<RoomPage />} />
       </Routes>
     </div>
   )
